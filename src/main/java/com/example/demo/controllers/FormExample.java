b7a707ce95c5580123bdbd6d6166704f4fa8c01e
Mads <mads.rosenlund@hotmail.com>
2021-02-22 15:36:27 +0100

    Kommentarer fra Benjamin

--- src/main/java/com/example/demo/controllers/FormExample.java
+++ src/main/java/com/example/demo/controllers/FormExample.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -9,10 +12,23 @@ import java.util.ArrayList;
 @Controller
 public class FormExample {
     // Showing how to create a form using thymeleaf
-    @GetMapping(value = "/form")
+    @GetMapping(value = "/submit")
     public String renderForm() {
         return "form";
     }
+    @PostMapping("/submit-post")
+    @ResponseBody
+    public String addPost(@RequestParam("title") String title,@RequestParam("content") String content){
+        return title + "\n" + content;
+        // Post post = new Post();
+        //posts.add(posts.add.title)
+
+
+    }
+
+
+
+    //Skal være dashboard
 
     @GetMapping(value="/list")
     @ResponseBody
